aa9e4001dc38bf8014e11aef91654920780a3c03
ducthanhdev <nguyenvantho083@gmail.com>
2025-09-21 04:26:59 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
   });
 
   await app.listen(process.env.PORT ?? 3000);
-  console.log(`🚀 Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
-  console.log(`📚 Swagger documentation: http://localhost:${process.env.PORT ?? 3000}/api`);
+  console.log(`Application is running on: http://localhost:${process.env.PORT ?? 3000}`);
+  console.log(`Swagger documentation: http://localhost:${process.env.PORT ?? 3000}/api`);
 }
 bootstrap();
